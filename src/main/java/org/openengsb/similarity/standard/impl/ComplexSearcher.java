db1c2422cbe1c6bc8fd0ed65590207face97df4d
Christoph Hochreiner <ch.hochreiner@gmail.com>
2012-02-19 00:39:41 +0100

    added sample for model based query

--- src/main/java/org/openengsb/similarity/standard/impl/ComplexSearcher.java
+++ src/main/java/org/openengsb/similarity/standard/impl/ComplexSearcher.java
@@ -3,6 +3,7 @@ package org.openengsb.similarity.standard.impl;
 import java.io.IOException;
 
 import org.openengsb.core.api.edb.EDBObject;
+import org.openengsb.similarity.standard.ConcreteModel;
 
 public class ComplexSearcher extends AbstractSearcher {
 
@@ -14,11 +15,14 @@ public class ComplexSearcher extends AbstractSearcher {
     protected String buildQueryString(EDBObject sample) {
         // ConcreteModel model = edbConverter.convertEDBObjectToModel(ConcreteModel.class, sample);
 
+        ConcreteModel model = null;
+
+        String modelBasedQuery = "complexKey:" + model.getKey1() + "#" + model.getKey2() + "#" + model.getKey3();
+
         String result = "";
         result +=
             "complexKey:" + sample.getString("key1") + "#" + sample.getString("key2") + "#" + sample.getString("key3");
 
         return result;
     }
-
 }
