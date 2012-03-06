9a579b43bf51c082974bb9ef1852f56dcec65b38
Christoph Hochreiner <ch.hochreiner@gmail.com>
2012-03-06 14:03:24 +0100

    added license files

--- src/main/java/org/openengsb/similarity/internal/PLCCombinedSearcher.java
+++ src/main/java/org/openengsb/similarity/internal/PLCCombinedSearcher.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openengsb.similarity.internal;
 
 import java.io.IOException;
@@ -12,11 +29,9 @@ public class PLCCombinedSearcher extends AbstractSearcher {
 
     @Override
     protected String buildQueryString(EDBObject sample) {
-        String result = "";
-        result += "key1:" + sample.getString("key1") + "~0.8 AND ";
-        result += "key2:" + sample.getString("key2") + "~0.8 AND ";
-        result += "key3:" + sample.getString("key3") + "~0.8";
 
+        String result =
+            "combined:" + sample.getString("key1") + "#" + sample.getString("key2") + "#" + sample.getString("key3");
         return result;
     }
 
